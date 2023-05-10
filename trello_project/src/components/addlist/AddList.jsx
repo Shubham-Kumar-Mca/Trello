7a2c11a2e1ad18f998c60b7a334ef13db7ff3a34
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-11 00:09:33 +0530

    Create Add List Button to add multiple List

---
+++ trello_project/src/components/addlist/AddList.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import "./addList.css";
+import { AiOutlinePlus } from "react-icons/ai"
+import { AuthContext } from '../../context/AuthContextProvider';
+
+const AddList = () => {
+    const { setIsToggle } = useContext(AuthContext)
+    return (
+        <div>
+            <div className='pluse__icon__with__txt' onClick={() => setIsToggle(true)}>
+                <AiOutlinePlus />
+                <span>Add a List</span>
+            </div>
+        </div>
+    )
+}
+
+export default AddList
