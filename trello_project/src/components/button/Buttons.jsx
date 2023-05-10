287dd98c4c1708daf18a5710d84f5fe963cdf7b3
Anuj Bhati <bhatianuj7247@gmail.com>
2023-05-11 00:41:10 +0530

    right section of desciption page

---
+++ trello_project/src/components/button/Buttons.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Buttons({Sign,logo,image,className,btnNext}) {
+  return (
+    <>
+      <button className={className} onClick={btnNext}>
+        {image}&nbsp;&nbsp;&nbsp;{Sign}&nbsp;&nbsp;&nbsp;{logo}
+      </button>
+    </>
+  );
+}
