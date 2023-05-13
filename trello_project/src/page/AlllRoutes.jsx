ad3605c3fc892f6b6af441e352d495c4fc748477
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-13 14:10:18 +0530

    Update Everything

--- trello_project/src/page/AlllRoutes.jsx
+++ trello_project/src/page/AlllRoutes.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Description from "./description/Description";
 import Home from './home/Home';
 const AlllRoutes = () => {
   return (
     <div>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/task/:id' element={<Description />} />
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/task/:id' element={<Description />} />
+      </Routes>
     </div>
   )
 };
