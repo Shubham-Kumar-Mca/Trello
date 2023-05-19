9cbef78895bc861f3937c7ce17df57255c00c44e
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-19 17:29:05 +0530

    Add List is completed

--- trello_project/src/components/addlist/AddList.jsx
+++ trello_project/src/components/addlist/AddList.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContextProvider';
 import { AiOutlinePlus } from "react-icons/ai";
-import "./addList.css";
+import "./AddList.css";
 
 const AddList = () => {
     const { setIsToggle } = useContext(AuthContext)
