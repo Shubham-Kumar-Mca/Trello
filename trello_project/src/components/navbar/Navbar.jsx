352a0e2e4029da9c644d2d141da9440fe1993b57
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-19 17:05:05 +0530

    done

--- trello_project/src/components/navbar/Navbar.jsx
+++ trello_project/src/components/navbar/Navbar.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <img width="64" height="64" src="https://img.icons8.com/nolan/64/react-native.png" alt="react-native"/>
+      <img width="64" height="64" src="https://img.icons8.com/nolan/64/react-native.png" alt="react-native" />
       <Link to="/" className="navbar__brand">
-    
         Kanban Board
       </Link>
-      
     </nav>
   );
 };
